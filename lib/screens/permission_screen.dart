915895dbc9849f528c70030349f8dc9ba2e56211
Aryan <aryanbisht9458@gmail.com>
2023-01-13 08:03:56 +0530

    Permission page.

---
+++ lib/screens/permission_screen.dart
@@ -0,0 +1,129 @@
+import 'package:book_my_taxi/Utils/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionScreen extends StatefulWidget {
+  const PermissionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PermissionScreen> createState() => _PermissionScreenState();
+}
+
+class _PermissionScreenState extends State<PermissionScreen> {
+  bool location = false, phone = true;
+
+  @override
+  void initState() {
+    super.initState();
+    readPermissions();
+  }
+
+  void readPermissions() async {
+    location = await Permission.location.isGranted;
+    phone = await Permission.contacts.isGranted;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Flexible(
+                child: Column(
+              children: [
+                SizedBox(
+                  height: 150,
+                ),
+                Icon(
+                  Icons.car_crash_sharp,
+                  size: 200,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "WELCOME TO BOOK MY ETAXI",
+                  style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Enjoy the safe & comfortable booking experience by giving us the following below Permissions",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.do_not_disturb_on_total_silence,
+                        size: 12, color: Colors.deepPurple),
+                    TextButton(
+                        onPressed: () async {
+                          var status = await Permission.location.request();
+                          if (status.isGranted) {
+                            location = true;
+                            context.showSnackBar(message: "Location Permission is Granted");
+                          }
+                        },
+                        child: Text(
+                          "Location (Please find the available rides)",
+                          style: _textStyle,
+                        ))
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.do_not_disturb_on_total_silence,
+                        size: 12, color: Colors.deepPurple),
+                    TextButton(
+                        onPressed: () async {
+                          var status = await Permission.contacts.request();
+                          if (status.isGranted){
+                            context.showSnackBar(message: "Contact Permission is Granted");
+                            phone = true;}
+                        },
+                        child: Text(
+                          "Phone (Security verification for account)",
+                          style: _textStyle,
+                        ))
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            )),
+            ElevatedButton(
+              onPressed: () {
+                //TODO onPressed work
+                if(location && phone){
+                  Navigator.of(context).pushNamed("/homeScreen");
+                }
+                else{
+                  context.showErrorSnackBar(message: "Please allow both the permission first");
+                }
+              },
+              child: Text("Allow"),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextStyle get _textStyle => const TextStyle(
+        color: Colors.black,
+        fontSize: 16,
+      );
+}
