41cd012058ec5643ea6f20c503b9076c3ed5c5e2
Aryan <aryanbisht9458@gmail.com>
2023-02-19 12:37:59 +0530

    Driver info screen is completed.

--- lib/screens/driver_info.dart
+++ lib/screens/driver_info.dart
@@ -1,5 +1,7 @@
 import 'package:book_my_taxi/Utils/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class DriverInfoScreen extends StatefulWidget {
   const DriverInfoScreen({Key? key}) : super(key: key);
@@ -13,6 +15,8 @@ class _DriverInfoScreenState extends State<DriverInfoScreen> {
   late String driverName;
   late String stars;
 
+  final LatLng _center = const LatLng(20.5937, 78.9629);
+
   @override
   void initState() {
     super.initState();
@@ -23,191 +27,207 @@ class _DriverInfoScreenState extends State<DriverInfoScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                "Your ride is confirmed",
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              Divider(
-                thickness: 2,
-                height: 10,
-                color: Colors.grey[300],
-              ),
-              const SizedBox(
-                height: 5,
-              ),
+    final panelHeightClosed = MediaQuery.of(context).size.height * 0.5;
 
-              //Driver Info
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return SafeArea(
+      child: SlidingUpPanel(
+        minHeight: panelHeightClosed,
+        panelBuilder: (controller) {
+          return Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  const Text(
+                    "Your ride is confirmed",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  Divider(
+                    thickness: 2,
+                    height: 10,
+                    color: Colors.grey[300],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+
+                  //Driver Info
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        vehicleNumber,
-                        style: const TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            vehicleNumber,
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          const Text("RE Compact Plus"),
+                          Row(
+                            children: [
+                              Text(driverName),
+                              const Icon(
+                                Icons.star,
+                                size: 16,
+                              ),
+                              Text(stars)
+                            ],
+                          )
+                        ],
                       ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Text("RE Compact Plus"),
                       Row(
                         children: [
-                          Text(driverName),
                           const Icon(
-                            Icons.star,
-                            size: 16,
+                            Icons.person_pin,
+                            size: 35,
                           ),
-                          Text(stars)
+                          Image.asset(
+                            "assets/images/mini.png",
+                          )
                         ],
                       )
                     ],
                   ),
+
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  //Call or message Row
                   Row(
                     children: [
-                      const Icon(
-                        Icons.person_pin,
-                        size: 35,
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: primaryColor,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.call,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                      Image.asset(
-                        "assets/images/mini.png",
-                      )
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                          child: TextField(
+                        decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                                onPressed: () {}, icon: const Icon(Icons.send)),
+                            border: const OutlineInputBorder(),
+                            hintText: "Message your driver..",
+                            hintStyle: const TextStyle(color: Colors.grey)),
+                      ))
                     ],
+                  ),
+
+                  Divider(
+                    thickness: 2,
+                    height: 10,
+                    color: Colors.grey[300],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: buttonRowLayout(),
+                  ),
+
+                  Divider(
+                    thickness: 2,
+                    height: 10,
+                    color: Colors.grey[300],
+                  ),
+                  //Cash row
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: const [
+                            Icon(Icons.money_outlined),
+                            Text("Cash"),
+                          ],
+                        ),
+                        const Text("Change")
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    thickness: 2,
+                    height: 10,
+                    color: Colors.grey[300],
+                  ),
+
+                  //Button rows
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.calculate,
+                          color: Colors.orange,
+                        ),
+                        label: const Text(
+                          "Fare Estimate",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          elevation: 0,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                        child: VerticalDivider(
+                          thickness: 2,
+                          width: 10,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.percent,
+                          color: Colors.orange,
+                        ),
+                        label: const Text(
+                          "Promo Code",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          elevation: 0,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[800]),
+                    child: const Text("CONFIRM BOOKING  "),
                   )
                 ],
               ),
-
-              const SizedBox(
-                height: 10,
-              ),
-              //Call or message Row
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: primaryColor,
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.call,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                      child: TextField(
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(onPressed: (){}, icon: const Icon(Icons.send)),
-                        border: const OutlineInputBorder(),
-                        hintText: "Message your driver..",
-                        hintStyle: const TextStyle(color: Colors.grey)),
-                  ))
-                ],
-              ),
-
-              Divider(
-                thickness: 2,
-                height: 10,
-                color: Colors.grey[300],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: buttonRowLayout(),
-              ),
-
-              Divider(
-                thickness: 2,
-                height: 10,
-                color: Colors.grey[300],
-              ),
-              //Cash row
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: const [
-                        Icon(Icons.money_outlined),
-                        Text("Cash"),
-                      ],
-                    ),
-                    const Text("Change")
-                  ],
-                ),
-              ),
-              Divider(
-                thickness: 2,
-                height: 10,
-                color: Colors.grey[300],
-              ),
-
-              //Button rows
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.calculate,
-                      color: Colors.orange,
-                    ),
-                    label: const Text(
-                      "Fare Estimate",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      elevation: 0,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                    child: VerticalDivider(
-                      thickness: 2,
-                      width: 10,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.percent,
-                      color: Colors.orange,
-                    ),
-                    label: const Text(
-                      "Promo Code",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      elevation: 0,
-                    ),
-                  ),
-                ],
-              ),
-
-              ElevatedButton(
-                onPressed: () {},
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.grey[800]),
-                child: const Text("CONFIRM BOOKING  "),
-              )
-            ],
+            ),
+          );
+        },
+        body: GoogleMap(
+          myLocationEnabled: true,
+          myLocationButtonEnabled: false,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: zoomLevel,
           ),
         ),
       ),
